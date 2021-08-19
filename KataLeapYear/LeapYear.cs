1721370cf6a6d725f3976b69497d61f54c8c1a6f
Rodrigo Fernandez <54083304+Fernandez-Rodrigo@users.noreply.github.com>
2021-08-19 10:30:52 -0300

    Add files via upload

---
+++ KataLeapYear/LeapYear.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLeapYear
+{
+    public class LeapYear
+    {
+
+
+        public bool IsLeap(int year)
+        {
+            /* if(year % 4 != 0 || year <= 0)
+             {
+                 return false;
+             }
+             else if(year % 400 != 0)
+             {
+                 return false;
+             }
+             else
+             {
+                 return true;
+             }*/
+            if (year >= 0)
+            { 
+               if(year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+               {
+                return true;
+               }
+            }
+            return false;
+   
+        }
+
+    }
+}
